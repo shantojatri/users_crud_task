426bc4d67140f9d5571af5a1442fa14a3609f1b3
shantojatri <shanto.jatri@gmail.com>
2024-03-31 18:11:38 +0600

    Added image uplaod trait

---
+++ app/Helpers/helpers.php
@@ -0,0 +1,38 @@
+<?php
+
+
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
+
+if (!function_exists('get_storage_image')) {
+
+    function get_storage_image($path, $name, $type = 'normal')
+    {
+        $full_path = '/storage/' . $path . '/' . $name;
+        if ($name) {
+            return asset($full_path);
+        }
+    }
+}
+
+
+if (!function_exists('log_error')) {
+
+    function log_error(\Exception $e)
+    {
+        Log::error($e->getMessage());
+    }
+}
+
+if (!function_exists('getImage')) {
+    function getImage($image = null, $type = null)
+    {
+        if ($image && Storage::disk('public')->exists($image)) {
+            return Storage::disk('public')->url($image);
+        } else {
+            return asset('/images/default.png');
+        }
+    }
+}
--- app/Traits/ImageUploadOrDeleteTraits.php
+++ app/Traits/ImageUploadOrDeleteTraits.php
@@ -4,8 +4,30 @@ namespace App\Traits\ImageUploadOrDeleteTraits;
 
 class ImageUploadOrDeleteTraits {
 
-    public function uploadImage(){
-        // image upload logic here
+    public function uploadImage($request, $inputName, $path, $old = null){
+        $filePath = '/app/public/'. $path .'/';
+
+        if ($request->hasFile($inputName)) {
+            $file = $request->file($inputName);
+
+            if ($old && $old->$inputName) {
+                try {
+                    unlink(storage_path($filePath) . $old->$inputName);
+                } catch (\Exception $e) {
+                }
+            }
+
+            $extension = $file->getClientOriginalExtension();
+            $imageName = uniqid() . '-' . time() . '.' . $extension;
+            $file->move(storage_path() . $filePath, $imageName);
+
+            return $imageName;
+        } else {
+            // When update without image
+            if ($old) {
+                return $old->$inputName;
+            }
+        }
     }
 
     public function deleteImage(){
